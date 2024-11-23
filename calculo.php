a4883edf9b5565551b589f500af75ee4945df36a
Andreia Luiza Mattos <71427210+andreialuizafm@users.noreply.github.com>
2024-11-23 15:04:45 +0000

    feat: implementar a integração entre o frontend e o backend

--- calculo.php
+++ calculo.php
@@ -1,23 +1,25 @@
 <?php
-
-//Diz ao PHP que vamos trabalhar usando protocolo JSON para a comunicação
-
+//Diz ao PHP que vamos trabalhar usando o protocolo JSON para comunicação
 header('Content-Type: application/json');
 
-//Verificamos se a requisição é do tipo POST
-
+//Verificamos se a requisição é do tipo POST,
+//Caso não seja, retornamos uma mensagem de erro
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
+    //filtramos os dados da requisição e 
+    //atribuímos à uma variável chamada $dados
     $dados = json_decode(file_get_contents("php://input"));
-    $areaComodo = $dados -> comodoComprimento * $dados -> pisoComprimento;
 
-    $areaPiso = $dados -> pisoLargura * $dados -> pisoComprimento;
+    $areaComodo = $dados->comodoLargura * $dados->comodoComprimento;
+    $areaPiso = $dados->pisoLargura * $dados->pisoComprimento;
 
     $quantidadePiso = $areaComodo / $areaPiso;
 
-    $margem = $quantidadePiso * ($dados -> $margem / 100);
-    
-    $quantidadeTotal = $quantidadePiso * $margem;
+    //10% = 0.1
+    //100m * 0.1 = 10m
+    $margem = $quantidadePiso * ($dados->margem / 100);
+
+    $quantidadeTotal = $quantidadePiso + $margem;
 
     echo json_encode([
         "areaComodo" => $areaComodo,
